689e97e4d3f1c5fcf15925d8ff13ec71923e3631
Tyler <Tyler.Hurtt@ibm.com>
2023-03-16 08:37:13 -0700

    add try catch

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,10 +21,11 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/', async (req, res) => {
     try {
-        const message = books;
-        res.status(200).json({ message });
+        const allBooks = await books
+        const message
+        res.status(200).json({ message: allBooks });
     } catch (error) {
-        res.status(400).json({ message });
+        res.status(400).json({ message: 'cannot get books' });
     }
 });
 
@@ -34,7 +35,6 @@ public_users.get('/isbn/:isbn', async (req, res) => {
     const { isbn } = req.params;
     const book = await books[isbn];
     return res.status(200).json({message: book});
-
   } catch(error) {
     res.status(404).json({message: 'book not found'});
   }
@@ -42,27 +42,40 @@ public_users.get('/isbn/:isbn', async (req, res) => {
   
 // Get book details based on author
 public_users.get('/author/:author', async (req, res) => {
-  const { author } = req.params;
-  const allBooks = await books
-  const booksArray = Object.values(allBooks);
-  const authorsBooks = booksArray.filter((b) => b.author === author);
-  return res.status(200).json({message: authorsBooks});
+  try {
+    const { author } = req.params;
+    const allBooks = await books
+    const booksArray = Object.values(allBooks);
+    const authorsBooks = booksArray.filter((b) => b.author === author);
+    return res.status(200).json({message: authorsBooks});
+  } catch(error) {
+    res.status(404).json({message: `books by ${author} could not be found`});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async (req, res) => {
-  const { title } = req.params;
-  const allBooks = await books;
-  const booksArray = Object.values(allBooks);
-  const titledBooks = booksArray.filter((b) => b.title === title);
-  return res.status(200).json({message: titledBooks});
+  try {
+    const { title } = req.params;
+    const allBooks = await books;
+    const booksArray = Object.values(allBooks);
+    const titledBooks = booksArray.filter((b) => b.title === title);
+    return res.status(200).json({message: titledBooks});
+  } catch(error) {
+    res.status(404).json({message: `books titled ${titled} could not be found`});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const { isbn } = req.params;
-  const { reviews } = books[isbn];
-  return res.status(200).json({message: reviews});
+  try {
+    const { isbn } = req.params;
+    const allBooks = await books;
+    const { reviews } = allBooks[isbn];
+    return res.status(200).json({message: reviews});
+  } catch(error) {
+    res.status(404).json({message: `book review of book with ISBN ${isbn} could not be found`});
+  }
 });
 
 module.exports.general = public_users;
